2a78985e7fb6dd57e6f9027405e829f6c166d6cb
João Victor <joaovictorsalotialves12@gmail.com>
2025-05-21 14:55:04 -0300

    Ajuste na tela minha estante para trasitar entre lidos e favoritos

--- src/app/(system)/(tabs)/bookshelf/index.tsx
+++ src/app/(system)/(tabs)/bookshelf/index.tsx
@@ -6,7 +6,6 @@ import { CategoryButton } from '../../_components/category-button'
 import { FeaturedBooks } from '../../_components/featured-books'
 import { GridBooks } from '../../_components/grid-books'
 import { NavigationHeader } from '../../_components/navigation-header'
-import { SearchButton } from '../../_components/search-button'
 
 import { useBooksIsReading } from '@/hooks/useBooksIsReading'
 import { useBooksReaded } from '@/hooks/useBooksReaded'
@@ -30,14 +29,63 @@ export default function Bookshelf() {
     getBooksReaded()
   }, [])
 
-  const isEmpty =
-    !booksIsReading?.length &&
-    !booksReaded?.length &&
-    !isLoadingBooksIsReading &&
-    !isLoadingBooksReaded
-
   const handleCategoryPress = (category: 'lidos' | 'favoritos') => {
     setSelectedCategory(category)
+  }
+
+  function ReadedBooks() {
+    const isEmpty =
+      !booksIsReading?.length &&
+      !booksReaded?.length &&
+      !isLoadingBooksIsReading &&
+      !isLoadingBooksReaded
+
+    return (
+      <>
+        {booksIsReading?.length > 0 && (
+          <FeaturedBooks title="Continuar lendo" data={booksIsReading} />
+        )}
+
+        {booksReaded?.length > 0 && (
+          <GridBooks title="Livros lidos" data={booksReaded} />
+        )}
+
+        {isEmpty && (
+          <View style={styles.emptyContainer}>
+            <Image
+              source={require('../../../../assets/logo_large.png')}
+              style={styles.emptyImage}
+              resizeMode="contain"
+            />
+            <Text style={styles.emptyText}>
+              Você ainda não leu nenhum livro
+            </Text>
+          </View>
+        )}
+      </>
+    )
+  }
+
+  function FavoriteBooks() {
+    const favorites = []
+    const isEmpty = favorites.length === 0
+
+    return (
+      <>
+        {isEmpty && (
+          <View style={styles.emptyContainer}>
+            <Image
+              source={require('../../../../assets/logo_large.png')}
+              style={styles.emptyImage}
+              resizeMode="contain"
+            />
+            <Text style={styles.emptyText}>
+              Você ainda não possui nenhum livro favorito
+            </Text>
+          </View>
+        )}
+      </>
+    )
   }
 
   return (
@@ -59,28 +107,8 @@ export default function Bookshelf() {
         </View>
 
         <View style={styles.body}>
-          <SearchButton />
-
-          {selectedCategory === 'lidos' && booksIsReading?.length > 0 && (
-            <FeaturedBooks title="Continuar lendo" data={booksIsReading} />
-          )}
-
-          {selectedCategory === 'lidos' && booksReaded?.length > 0 && (
-            <GridBooks title="Livros lidos" data={booksReaded} />
-          )}
-
-          {isEmpty && (
-            <View style={styles.emptyContainer}>
-              <Image
-                source={require('../../../../assets/logo_large.png')}
-                style={styles.emptyImage}
-                resizeMode="contain"
-              />
-              <Text style={styles.emptyText}>
-                Você ainda não leu nenhum livro
-              </Text>
-            </View>
-          )}
+          {selectedCategory === 'lidos' && <ReadedBooks />}
+          {selectedCategory === 'favoritos' && <FavoriteBooks />}
         </View>
       </View>
     </KeyboardAwareContainer>
